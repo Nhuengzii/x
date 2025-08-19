33d37bf29081dcd495e9baa03abc23e4e2e75b20
Anawat Moonmanee <32nhueng@gmail.com>
2025-08-19 14:54:34 +0700

    add var

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+variable "file_content" {
+  description = "Content for the hello.txt file"
+  type        = string
+  default     = "Hello from Terraform variable!"
+}
+
 resource "null_resource" "local_provision" {
   provisioner "local-exec" {
     command = "echo 'Hello World from Terraform local provisioner'"
